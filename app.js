1bafcecd7403209c253f7c2d311209276e75b93a
Andrea C <mirthis@gmail.com>
2022-04-05 20:46:25 +0100

    retest deploy pipeline

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/ping', (req, res) => {
-  res.send('pong') // change this string to ensure a new version deployed
+  res.send('pong!') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
